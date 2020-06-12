e48a0b5740778fb243cf5475f6a14579837a21bd
Kelvyn Quirino da Costa <kelvyn.costa@zema.com>
2020-06-12 13:02:51 -0300

    Initial Commit

---
+++ src/routes.js
@@ -0,0 +1,40 @@
+import { Router } from 'express';
+import multer from 'multer';
+
+import multerConfig from './config/multer';
+import UserController from './app/controllers/user';
+import SessionController from './app/controllers/session';
+import FileController from './app/controllers/file';
+import ProviderController from './app/controllers/provider';
+import AppointmentController from './app/controllers/appointment';
+import ScheduleController from './app/controllers/schedule';
+import NotificationController from './app/controllers/notification';
+import AvailableController from './app/controllers/available';
+
+import authMiddleware from './app/middlewares/auth';
+
+const routes = new Router();
+const upload = multer(multerConfig);
+
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
+
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.delete);
+
+routes.get('/schedules', ScheduleController.index);
+
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
+
+export default routes;
